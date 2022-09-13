ecbe547bc77538eb2ca9515ac5b1835a516e3424
martinopolot <maopolot@outlook.com>
2022-09-13 16:21:25 -0700

    'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - display lowercase alphabet - by Martin- Sept
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int k;
+
+	for (k = 'a'; k <= 'z'; i++)
+
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 # 0x02. C - Functions, nested loops
 Write a program that prints _putchar, followed by a new line.
+Write a function that prints the alphabet, in lowercase, followed by a new line.
