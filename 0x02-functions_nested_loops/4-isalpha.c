4972560d8df925b62a99553f07f9ab9f359958dc
martinopolot <maopolot@outlook.com>
2022-09-13 21:54:29 -0700

    isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 /**
  * _isalpha - Checking whether alphabet
  *  contains is a charachter - return 1 for  a leter and 0 if not
- *
+ * @c: type int character
  *  Return: 1 if letter
  */
 int _island(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'A'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
