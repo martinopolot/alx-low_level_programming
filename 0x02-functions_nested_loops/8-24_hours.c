6f2d4e1f9b85edaef7fdc925d2ccdd0e93853b6e
martinopolot <maopolot@outlook.com>
2022-09-14 00:00:23 -0700

    jack_bauer's time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int b, m;
+
+	for (b = 0; b < 24; b++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(b / 10 + 48);
+			_putchar(b % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
