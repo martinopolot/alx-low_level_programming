26a96d2b9f8f17f6ef72556f033101f28813bdd4
martinopolot <maopolot@outlook.com>
2022-09-14 01:36:07 -0700

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - func to print nural numbers
  * @n: int n
@@ -9,10 +10,10 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (n = 0; n < 97; n++)
-			printf("%d, ", n);
+			printf("%d\n", n);
 	}
 	else
-		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
-	printf("98\n");
+		for (n = n; n <= 97; n--)
+			printf("%d\n", n);
+
 }
