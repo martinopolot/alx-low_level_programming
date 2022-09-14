4b03d5922068593dc3a08ae68cdc4208c0fd0541
martinopolot <maopolot@outlook.com>
2022-09-14 03:12:28 -0700

    Corrected -isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * _isalpha - Checking whether alphabet
- *  contains is a charachter - return 1 for  a leter and 0 if not
- * @c: type int character
- *  Return: 1 if letter
+ * _isalpha - returns 1 if char c is alphabet
+ * @c: character
+ * Return: 1 if alphabet, 0 if not.
  */
 int _island(int c)
 {
-	if ((c >= '65' && c <= '90') || (c >= '97' && c <= '122'))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
