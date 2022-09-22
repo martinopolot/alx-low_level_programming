8e95f7d434b66fb10ab4268d0414f86315d5f375
martinopolot <maopolot@outlook.com>
2022-09-22 13:54:59 -0700

    to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - function to convert string to uppercase
+ * @s: input string
+ * Return: points to destination
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) < 122))
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+
+	return (s);
+}
